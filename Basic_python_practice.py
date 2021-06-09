92feb08d9f15723159636361b72dd8adac6c75ec
JothishR <j.eeshwar28@gmail.com>
2021-06-09 10:48:37 +0530

    Initial commit

---
+++ Basic_python_practice.py
@@ -0,0 +1,9 @@
+def completed():
+    print("--------------------------------x--------------------------------------")
+def output():
+    print("________ OUTPUT _________")
+def error():
+    print("xxxxxxxxxxx ERROR xxxxxxxxxxxx")
+
+# Start practice from here
+
