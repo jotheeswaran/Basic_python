12ade78254ce212fad94130ee8abdd6df260d5ec
jothishr <j.eeshwar29@gmail.com>
2021-06-12 08:07:03 +0530

    small practices

--- Basic_python_practice.py
+++ Basic_python_practice.py
@@ -6,4 +6,24 @@ def error():
     print("xxxxxxxxxxx ERROR xxxxxxxxxxxx")
 
 # Start practice from here
+#1.addition
+"""completed()
+output()
+print(2+5)"""
+#________ OUTPUT _________
+#7
+#--------------------------------x--------------------------------------
 
+#2.diclare the variable:
+
+"""output()
+a=5
+b=6
+c=a+b
+print(c)
+completed()"""
+#________ OUTPUT _________
+#11
+#--------------------------------x--------------------------------------
+
+3.
